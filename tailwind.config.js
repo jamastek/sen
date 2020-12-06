46c195541f077be97bb5e7190467790b4360bb6b
Muhammad Rusdi <mhmrofficial@gmaill.com>
2020-12-06 13:38:51 +0800

    fix container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   purge: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "1.5rem"
+    }
   },
   variants: {
     extend: {},
