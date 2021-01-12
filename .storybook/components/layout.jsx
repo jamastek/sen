e63bd562c4a069d647ee3049653feb847c1a1773
muhrusdi <mhmrofficial@gmail.com>
2021-01-13 00:07:35 +0800

    add storybookjs

---
+++ .storybook/components/layout.jsx
@@ -0,0 +1,11 @@
+import "../../styles/globals.css"
+
+const Layout = ({children}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default Layout
--- components/button/index.tsx
+++ components/button/index.tsx
@@ -1,7 +1,30 @@
 const Button = (props) => {
   return (
-    <button className="p-4 bg-blue-600" {...props}>test</button>
+    <button className="p-4 bg-blue-600 rounded-md" {...props}>test</button>
   )
 }
 
 export default Button
+
+export interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
