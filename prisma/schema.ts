aa40821f648eb63e55a4ee302ecdbdc0a9490883
muhrusdi <mhmrofficial@gmail.com>
2021-01-14 13:56:30 +0800

    change structure

--- prisma/schema.ts
+++ prisma/schema.ts
@@ -1,12 +1,12 @@
 import { makeSchema } from 'nexus'
 import path from 'path'
-import { Query, Mutation, Post, User, GQLDate } from "./object-types"
+import { Query, Mutation, Post, User, GQLDate } from "./nexus"
 
 const schema = makeSchema({
   types: [Query, Mutation, Post, User, GQLDate],
   outputs: {
-    typegen: path.join(process.cwd(), 'prisma/nexus/nexus-typegen.ts'),
-    schema: path.join(process.cwd(), 'prisma/nexus/schema.graphql'),
+    typegen: path.join(process.cwd(), 'prisma/generated/nexus-typegen.ts'),
+    schema: path.join(process.cwd(), 'prisma/generated/schema.graphql'),
   },
 })
 
