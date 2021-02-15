18dbc31469fccabe507dc584f0a261321ccd279d
muhrusdi <mhmrofficial@gmail.com>
2021-02-16 03:11:45 +0800

    fix post css

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,18 @@
 import { addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import Layout from "./components/layout"
+import { DocsContainer } from '@storybook/addon-docs/blocks';
+import { MDXEmbedProvider } from 'mdx-embed';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  docs: {
+    container: ({ children, context }) => (
+      <DocsContainer context={context}>
+        <MDXEmbedProvider>{children}</MDXEmbedProvider>
+      </DocsContainer>
+    ),
+  },
 }
 
 addDecorator(storyFn => <Layout>{storyFn()}</Layout>);
